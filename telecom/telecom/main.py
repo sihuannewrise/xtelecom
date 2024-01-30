3014e17a902dabfc0037db7cb047b657bc2d4590
sihuannewrise <sihuannewrise@gmail.com>
2024-01-30 16:00:33 +0300

    minor changes

--- telecom/telecom/main.py
+++ telecom/telecom/main.py
@@ -8,11 +8,29 @@ xtelecom = FastAPI(
 )
 
 
-@xtelecom.get("/")
+@xtelecom.get(
+    "/",
+    tags=["special methods", "xtelecom"],
+    summary="Приветствие автора",
+    response_description="Описание ответа",
+)
 def read_root() -> dict[str, str]:
+    """
+    вместо description - Приветствие пользователя:
+
+    - **name**: имя
+    - **surname**: фамилия
+    - **age**: возраст (опционально)
+    - **is_staff**: является ли пользователь сотрудником
+    - **education_level**: уровень образования (опционально)
+    """
     return {"hello": "telecom"}
 
 
-@xtelecom.get("/config", tags=["settings"])
+@xtelecom.get(
+    "/config",
+    tags=["settings", "xtelecom"],
+    summary="Приветствие не автора",
+)
 def read_config() -> dict[str, str]:
     return {"model_dump": "Settings().model_dump()"}
